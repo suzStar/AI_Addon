f931b29528eb8cf7b0e6994e69fb79bf19784598
suzStar <114248187+suzStar@users.noreply.github.com>
2025-06-09 10:37:40 +0100

    Added basic JS for form handing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem-output", {
+    strings: ["Purple petals sway"],
+    autoStart: true,
+  });
+}
+
+let poemForm = document.querySelector("#poem-input");
+poemForm.addEventListener("submit", generatePoem);
